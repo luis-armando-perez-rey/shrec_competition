a8cb8458efeac9f5f3dee4fcfdb668e374639d0a
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2020-05-04 10:04:02 +0200

    Updated the image rendering files

--- rendering/render_rotations.rb
+++ rendering/render_rotations.rb
@@ -11,7 +11,12 @@ y_resolution = 256
 # Select directory with SKP models interactively 
 dir = UI.select_directory(title: "Select directory of .skp models")
 files = Dir.entries(dir).select{|e| e =~ /[.]skp$/ }
+number_files = Array.new
 puts(files)
+for file in files
+	number_files.insert(-1,file.split(".skp")[0])
+end
+puts(number_files.length)
 
 # Saving directory 
 save_dir = UI.select_directory(title: "Select directory for saving renders")
@@ -23,17 +28,41 @@ m = (0...num_views).to_a
 pi_fraction = 2*Math::PI/num_views
 views = m.map { |a| pi_fraction*a }
 
-for file in files
-	skp_file_path = dir+"/"+file
+
+# Identify previously created images 
+created_images = Dir.entries(save_dir).select{|e| e =~ /[.]png$/ }
+models = Array.new
+for image_file in created_images
+	models.insert(-1, image_file.split("_")[0])
+end
+# Unique 3D scenes that have been rendered
+unique_models = models.uniq
+puts(unique_models.length)
+# 3D model scene names that haven't been rendered
+difference = unique_models - number_files | number_files - unique_models  
+
+puts("Number of 3D models that will be rendered"+difference.length.to_s)
+
+for file in difference
+	skp_file_path = dir+"/"+file+".skp"
 	# Important constants
 	puts("Loading "+skp_file_path)
 	
 	
 	# Load sketchup model
-	path = dir+"/"+file
-	Sketchup.open_file(path)
+	Sketchup.open_file(skp_file_path)
 
 
+	filename = file.split(".skp")[0]
+	
+	Sketchup.active_model.active_view.write_image({
+	        :filename    => save_dir+"/"+filename+"_"+String(12)+"_.png",
+        	:antialias   => true,
+        	:transparent => true,
+		:width => x_resolution,
+		:height => y_resolution})
+	
+	
 	ents = Sketchup.active_model.entities
 	group=ents.add_group(Sketchup.active_model.entities.to_a)
 	ctr = group.bounds.center
@@ -47,7 +76,6 @@ for file in files
 	ents.transform_entities t, group
 	puts("Loaded "+skp_file_path)
 
-	
 
 	for view in 0...num_views 
 		puts("Rendering view "+String(view))
@@ -55,7 +83,7 @@ for file in files
 		eye = [radius*(Math::cos(theta)*Math::sin(phi)), radius*Math::sin(theta)*Math::sin(phi), group_height]
 		cam = Sketchup::Camera.new eye, [0,0,0], [0,0,1]
 		camera_view.camera = cam
-		filename = file.split(".skp")[0]
+		
 		Sketchup.active_model.active_view.write_image({
 	        	:filename    => save_dir+"/"+filename+"_"+String(view)+"_.png",
         		:antialias   => true,
